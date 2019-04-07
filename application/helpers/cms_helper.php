aa8a6e9360009dcad22345843280771dc9c4d30c
DZywolf <dzywolf@impure.design>
2019-04-07 11:59:08 -0300

    Cleanup

--- application/helpers/cms_helper.php
+++ application/helpers/cms_helper.php
@@ -1,16 +1,18 @@
-<?php if(! defined('BASEPATH')) exit ('No direct script access allowed');
+<?php
+if(!defined('BASEPATH')) exit ('No direct script access allowed');
 
-if(! function_exists('cms_helper')){
+if(!function_exists('cms_helper'))
+{
+    function cms_helper()
+    {
+        $CI =& get_instance();
 
-  function cms_helper(){
-    $CI =& get_instance();
-
-    if(!$CI->m_modules->getInstallationStatus()){
-      if (file_exists("install/index.php")):
-          header("Location: install");
-          die();
-      endif;
+        if(!$CI->m_modules->getInstallationStatus())
+        {
+            if(file_exists("install/index.php")):
+                header("Location: install");
+                die();
+            endif;
+        }
     }
-  }
-
 }
