bac90625975bb563e25d87b8cd97290c6a553f58
DZywolf <dzywolf@impure.design>
2019-05-07 15:31:59 -0400

    Set Dynamic base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
+$protocol = is_https() ? 'https://' : 'http://';
+$config['base_url'] = $protocol.$_SERVER['HTTP_HOST'].'/';
 
 /*
 |--------------------------------------------------------------------------
