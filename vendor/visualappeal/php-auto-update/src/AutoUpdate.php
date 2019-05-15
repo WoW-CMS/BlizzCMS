d9be175d15439bce588e3a7355eca5593dcd3ad0
Thranrion <thranion@wow-cms.com>
2019-05-15 03:45:57 +0200

    [FIX/CURL] SSL CURL ERROR

--- vendor/visualappeal/php-auto-update/src/AutoUpdate.php
+++ vendor/visualappeal/php-auto-update/src/AutoUpdate.php
@@ -582,33 +582,33 @@ class AutoUpdate
      * @param string $url URL to file
      * @return string|false
      */
-    protected function _downloadCurl($url)
-    {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $this->sslVerifyHost);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $this->sslVerifyHost);
-        $update = curl_exec($curl);
-
-        $error = false;
-        if (curl_error($curl)) {
-            $error = true;
-            $this->log->addError(sprintf(
-                'Could not download update "%s" via curl: %s!',
-                $url,
-                curl_error($curl)
-            ));
-        }
-        curl_close($curl);
-
-        if ($error === true) {
-            return false;
-        }
-
-        return $update;
-    }
+     protected function _downloadCurl($url)
+     {
+         $curl = curl_init();
+         curl_setopt($curl, CURLOPT_URL, $url);
+         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+         if ($this->sslVerifyHost) {
+             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+         } else {
+             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+         }
+         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $this->sslVerifyHost);
+         $update = curl_exec($curl);
+         $error = false;
+         if (curl_error($curl)) {
+             $error = true;
+             $this->log->addError(sprintf(
+                 'Could not download update "%s" via curl: %s!',
+                 $url,
+                 curl_error($curl)
+             ));
+         }
+         curl_close($curl);
+         if ($error === true) {
+             return false;
+         }
+         return $update;
+     }
 
     /**
      * Download the update
