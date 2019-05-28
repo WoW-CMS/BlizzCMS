8f9618c95968d806ec959c78eae9134c9febf24c
DZywolf <dzywolf@impure.design>
2019-05-28 00:54:29 -0400

    Cleanup

--- application/modules/donate/controllers/Donate.php
+++ application/modules/donate/controllers/Donate.php
@@ -59,9 +59,6 @@ class Donate extends MX_Controller
 
         if(!$this->wowauth->isLogged())
             redirect(base_url('login'),'refresh');
-
-        if ($this->wowauth->checkAccountExist() == 0)
-            redirect(base_url('panel'),'refresh');
     }
     
     public function index()
