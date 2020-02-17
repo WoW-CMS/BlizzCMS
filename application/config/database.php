cec7f4ca09eda9a467cc4905d5d9921f3d0bdc8a
Thranrion <duelista16@gmail.com>
2020-02-17 12:44:27 +0000

    Revert "Reparados archivos no trackeados"
    This reverts commit 9419bc5f48af486a73832935a158ee1062db45f6

---
+++ application/config/database.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+    'hostname' => '%HOSTNAME%',
+	'username' => '%USERNAME%',
+	'password' => '%PASSWORD%',
+	'database' => '%DATABASE%',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['auth'] = array(
+	'dsn'	=> '',
+    'hostname' => '%HOSTNAME%',
+	'username' => '%USERNAME%',
+	'password' => '%PASSWORD%',
+	'database' => '%DATABASE%',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
