1dc19fc58867befe293eb54c4508fa1fef0f2de1
DZywolf <dzywolf@zynon.stream>
2020-11-22 14:59:03 -0300

    Add languages to system

---
+++ system/language/thai/email_lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['email_must_be_array'] = 'การตรวจสอบความถูกต้องของอีเมลจะรับค่าเป็นอาร์เรย์';
+$lang['email_invalid_address'] = 'ที่อยู่อีเมลไม่ถูกต้อง: %s';
+$lang['email_attachment_missing'] = 'ไม่พบเอกสารแนบอีเมลต่อไปนี้:% s';
+$lang['email_attachment_unreadable'] = 'ไม่สามารถเปิดเอกสารแนบนี้ได้: %s';
+$lang['email_no_from'] = 'ไม่สามารถส่งอีเมลที่ไม่ระบุอีเมลของผู้ส่ง';
+$lang['email_no_recipients'] = 'คุณต้องระบุอีเมลผู้รับด้วย: ถึง, สำเนาถึง หรือ สำเนาลับถึง';
+$lang['email_send_failure_phpmail'] = 'ไม่สามารถส่งอีเมลโดยใช้ PHP mail(). เซิร์ฟเวอร์ของคุณอาจไม่ได้รับการตั้งค่าให้ทำการส่งอีเมลด้วยวิธีนี้.';
+$lang['email_send_failure_sendmail'] = 'ไม่สามารถส่งอีเมลโดยใช้ PHP Sendmail. เซิร์ฟเวอร์ของคุณอาจไม่ได้รับการตั้งค่าให้ทำการส่งอีเมลด้วยวิธีนี้.';
+$lang['email_send_failure_smtp'] = 'ไม่สามารถส่งอีเมลโดยใช้ PHP SMTP. เซิร์ฟเวอร์ของคุณอาจไม่ได้รับการตั้งค่าให้ทำการส่งอีเมลด้วยวิธีนี้.';
+$lang['email_sent'] = 'ข้อความของคุณถูกส่งออกเรียบร้อยแล้วด้วยโปรโตคอลนี้: %s';
+$lang['email_no_socket'] = 'ไม่สามารถทำการเปิด Socket เพื่อใช้งาน Sendmail. กรุณาตรวจสอบการตั้งค่า';
+$lang['email_no_hostname'] = 'คุณไม่ได้ระบุ SMTP hostname.';
+$lang['email_smtp_error'] = 'เกิดข้อผิดพลาดของ SMTP ดังต่อไปนี้: %s';
+$lang['email_no_smtp_unpw'] = 'ผิดพลาด: คุณต้องกำหนดชื่อผู้ใช้งาน และ รหัสผ่าน ของ SMTP';
+$lang['email_failed_smtp_login'] = 'การส่งคำสั่ง AUTH LOGIN ล้มเหลว. ข้อผิดพลาด: %s';
+$lang['email_smtp_auth_un'] = 'การยืนยันชื่อผู้ใช้งานล้มเหลว. ข้อผิดพลาด: %s';
+$lang['email_smtp_auth_pw'] = 'การยืนยันรหัสผ่านล้มเหลว. ข้อผิดพลาด: %s';
+$lang['email_smtp_data_failure'] = 'ไม่สามารถส่งข้อมูล: %s';
+$lang['email_exit_status'] = 'จบการทำงานด้วยรหัสสถานะ: %s';
---
+++ system/language/thai/upload_lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2018, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['upload_userfile_not_set'] = 'ไม่สามารถค้นหาตัวแปรโพสต์ที่ชื่อ userfile';
+$lang['upload_file_exceeds_limit'] = 'ไฟล์ที่อัปโหลดมีขนาดเกินขนาดสูงสุดที่อนุญาตจากการตั้งค่า PHP ของคุณ';
+$lang['upload_file_exceeds_form_limit'] = 'ไฟล์ที่อัปโหลดเกินขนาดสูงสุดได้รับอนุญาตจากฟอร์ม';
+$lang['upload_file_partial'] = 'ไฟล์ถูกอัปโหลดเพียงบางส่วนเท่านั้น';
+$lang['upload_no_temp_directory'] = 'ไม่พบโฟลเดอร์ชั่วคราว';
+$lang['upload_unable_to_write_file'] = 'ไม่สามารถเขียนไฟล์ไปยังดิสก์';
+$lang['upload_stopped_by_extension'] = 'การอัปโหลดไฟล์ถูกหยุดไว้โดย PHP extension.';
+$lang['upload_no_file_selected'] = 'คุณยังไม่ได้เลือกไฟล์ที่จะอัปโหลด';
+$lang['upload_invalid_filetype'] = 'ประเภทของไฟล์ที่คุณกำลังพยายามที่จะอัปโหลดนั้นไม่ได้รับอนุญาต';
+$lang['upload_invalid_filesize'] = 'ไฟล์ที่คุณกำลังพยายามที่จะอัปโหลด มีขนาดใหญ่กว่าขนาดที่ได้รับอนุญาต';
+$lang['upload_invalid_dimensions'] = 'ภาพที่คุณกำลังพยายามที่จะอัปโหลด มีขนาดไม่พอดีกับขนาดที่ได้รับอนุญาต';
+$lang['upload_destination_error'] = 'พบข้อผิดพลาดขณะกำลังย้ายไฟล์ที่อัปโหลดไปยังที่อยู่ปลายทาง';
+$lang['upload_no_filepath'] = 'ที่อยู่ไฟล์ที่จะอัปโหลดไม่ถูกต้อง';
+$lang['upload_no_file_types'] = 'คุณไม่ได้ระบุชนิดของไฟล์ที่ได้รับอนุญาต';
+$lang['upload_bad_filename'] = 'ชื่อไฟล์ที่คุณส่งมานั้นมีอยู่แล้วบนเซิร์ฟเวอร์';
+$lang['upload_not_writable'] = 'ไม่สามารถเขียนไฟล์ไปยังโฟลเดอร์ปลายทางที่ต้องการอัปโหลดได้';
