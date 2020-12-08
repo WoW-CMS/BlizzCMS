5a50d172854b5946d0ed11c3fbf318708a745e12
DZywolf <dzywolf@zynon.stream>
2020-12-08 02:35:31 -0300

    New validation rules

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * BlizzCMS
+ *
+ * @author  WoW-CMS
+ * @copyright  Copyright (c) 2017 - 2020, WoW-CMS.
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link    https://wow-cms.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_alpha_numeric_special'] = "The {field} field may only contain alpha-numeric characters, spaces and ~ ! # $ % & [ ] * - _ + = | : . ' characters";
+$lang['form_validation_update_unique'] = 'This {field} already exists.';
+$lang['form_validation_validate_captcha'] = 'This {field} is not valid.';
---
+++ application/libraries/MY_Form_validation.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * BlizzCMS
+ *
+ * @author  WoW-CMS
+ * @copyright  Copyright (c) 2017 - 2020, WoW-CMS.
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link    https://wow-cms.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Form_validation extends CI_Form_validation
+{
+	/**
+	 * Reference to the CodeIgniter instance
+	 *
+	 * @var object
+	 */
+	public $CI;
+
+	public function __construct($rules = [])
+	{
+		parent::__construct($rules);
+	}
+
+	/**
+	 * Alphanumeric characters, spaces, and  ~ ! # $ % & [ ] * - _ + = | : . characters
+	 *
+	 * @param string
+	 * @return bool
+	 */
+	public function alpha_numeric_special($str)
+	{
+		return (bool) preg_match('/^[A-Z0-9 ~!#$%\&\[\]\*\-_+=|:.]+$/i', $str);
+	}
+
+	/**
+	 * Update Unique
+	 *
+	 * @param string $str
+	 * @param string $field
+	 * @return bool
+	 */
+	public function update_unique($str, $field)
+	{
+		sscanf($field, '%[^.].%[^.].%[^.]', $table, $field, $id);
+		return is_object($this->CI->db)
+			? ($this->CI->db->limit(1)->get_where($table, array($field => $str, 'id !=' => $id))->num_rows() === 0)
+			: FALSE;
+	}
+
+	/**
+	 * Validate Captcha
+	 *
+	 * @param string $str
+	 * @return bool
+	 */
+	public function validate_captcha($str)
+	{
+		$private = $this->CI->config->item('captcha_private');
+		$type    = $this->CI->config->item('captcha_type');
+
+		$url = ($type == 'hcaptcha') ? 'https://hcaptcha.com/siteverify' : 'https://www.google.com/recaptcha/api/siteverify';
+		$data = http_build_query(['secret' => $private, 'response' => $str]);
+
+		// Send POST Request
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		// Decode Response
+		$decode = json_decode($response, true);
+
+		if ($decode['success'])
+		{
+			return true;
+		}
+
+		return false;
+	}
+}
