b2e9390d2296a6bc58694d4225f52230cf348da6
DZywolf <dzywolf@zynon.stream>
2020-12-10 19:10:55 -0300

    Add helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array('cache');
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'text', 'form', 'html', 'language', 'string', 'date', 'values', 'mod');
+$autoload['helper'] = array('url', 'file', 'text', 'form', 'html', 'language', 'directory', 'string', 'date', 'values', 'mod');
 
 /*
 | -------------------------------------------------------------------
