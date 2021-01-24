05758e320538fc9cce80ff2be1396636ec2e3290
Pagani Walter <paganiwalter@gmail.com>
2021-01-24 13:53:15 -0300

    Update login

--- application/modules/user/views/login1.php
+++ application/modules/user/views/login1.php
@@ -4,16 +4,16 @@
     <section class="uk-section uk-section-xsmall main-section" data-uk-height-viewport="expand: true">
       <div class="uk-container">
         <h4 class="uk-h4 uk-heading-line uk-text-uppercase uk-text-bold uk-margin-small-bottom"><span><i class="fas fa-sign-in-alt"></i> <?= $this->lang->line('button_login'); ?></span></h4>
-        <?php if($this->session->flashdata('account_activation') == 'true'): ?>
-        <div class="uk-alert-success" uk-alert>
-          <a class="uk-alert-close" uk-close></a>
-          <p><i class="far fa-check-circle"></i> <span class="uk-text-bold"><?= $this->lang->line('notification_valid_key'); ?></span>. <?= $this->lang->line('notification_valid_key_desc'); ?></p>
-        </div>
-        <?php elseif($this->session->flashdata('account_activation') == 'false'): ?>
-        <div class="uk-alert-danger" uk-alert>
-          <a class="uk-alert-close" uk-close></a>
-          <p><i class="far fa-times-circle"></i> <?= $this->lang->line('notification_invalid_key'); ?></p>
-        </div>
+        <?php if ($this->session->flashdata('account_activation') == 'true') : ?>
+          <div class="uk-alert-success" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            <p><i class="far fa-check-circle"></i> <span class="uk-text-bold"><?= $this->lang->line('notification_valid_key'); ?></span>. <?= $this->lang->line('notification_valid_key_desc'); ?></p>
+          </div>
+        <?php elseif ($this->session->flashdata('account_activation') == 'false') : ?>
+          <div class="uk-alert-danger" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            <p><i class="far fa-times-circle"></i> <?= $this->lang->line('notification_invalid_key'); ?></p>
+          </div>
         <?php endif; ?>
         <?= form_open('', 'id="loginForm" onsubmit="LoginForm(event)"'); ?>
         <div class="uk-margin" uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-inline; delay: 300; repeat: true">
@@ -34,10 +34,10 @@
         </div>
         <div class="uk-grid uk-grid-small uk-grid-margin-small" data-uk-grid>
           <div class="uk-width-1-4@m">
-            <?php if($this->wowmodule->getreCaptchaStatus() == '1'): ?>
-            <div class="uk-margin-small">
-              <div class="g-recaptcha" data-sitekey="<?= $recapKey; ?>"></div>
-            </div>
+            <?php if ($this->wowmodule->getreCaptchaStatus() == '1') : ?>
+              <div class="uk-margin-small">
+                <div class="g-recaptcha" data-sitekey="<?= $recapKey; ?>"></div>
+              </div>
             <?php endif; ?>
             <a href="<?= base_url('recovery'); ?>" class="uk-button uk-button-text"><i class="fas fa-key"></i> <?= $this->lang->line('button_forgot_password'); ?></a>
           </div>
@@ -56,22 +56,14 @@
 
         var restatus = "<?= $this->wowmodule->getreCaptchaStatus(); ?>";
 
-        if(restatus){
+        if (restatus) {
           var ren = grecaptcha.getResponse();
-          if(ren.length == 0)
-          {
-            $.amaran({
-              'theme': 'awesome error',
-              'content': {
-                title: '<?= $this->lang->line('notification_title_error'); ?>',
-                message: '<?= $this->lang->line('notification_captcha_error'); ?>',
-                info: '',
-                icon: 'fas fa-shield-alt'
-              },
-              'delay': 5000,
-              'position': 'top right',
-              'inEffect': 'slideRight',
-              'outEffect': 'slideRight'
+          if (ren.length == 0) {
+            Swal.fire({
+              icon: 'error',
+              title: '<?= $this->lang->line('notification_title_error'); ?>',
+              text: '<?= $this->lang->line('notification_captcha_error'); ?>',
+              showConfirmButton: true,
             });
             return false;
           }
@@ -79,93 +71,69 @@
 
         var username = $('#login_username').val();
         var password = $('#login_password').val();
-        if(username == ''){
-          $.amaran({
-            'theme': 'awesome error',
-            'content': {
-              title: '<?= $this->lang->line('notification_title_error'); ?>',
-              message: '<?= $this->lang->line('notification_username_empty'); ?>',
-              info: '',
-              icon: 'fas fa-times-circle'
-            },
-            'delay': 5000,
-            'position': 'top right',
-            'inEffect': 'slideRight',
-            'outEffect': 'slideRight'
+
+        if (username == '') {
+          Swal.fire({
+            icon: 'error',
+            title: '<?= $this->lang->line('notification_title_error'); ?>',
+            text: '<?= $this->lang->line('notification_username_empty'); ?>',
+            showConfirmButton: true,
           });
           return false;
         }
-        if(password == ''){
-          $.amaran({
-            'theme': 'awesome error',
-            'content': {
-              title: '<?= $this->lang->line('notification_title_error'); ?>',
-              message: '<?= $this->lang->line('notification_password_empty'); ?>',
-              info: '',
-              icon: 'fas fa-times-circle'
-            },
-            'delay': 5000,
-            'position': 'top right',
-            'inEffect': 'slideRight',
-            'outEffect': 'slideRight'
+
+        if (password == '') {
+          Swal.fire({
+            icon: 'error',
+            title: '<?= $this->lang->line('notification_title_error'); ?>',
+            text: '<?= $this->lang->line('notification_password_empty'); ?>',
+            showConfirmButton: true,
           });
           return false;
         }
+
         $.ajax({
-          url:"<?= base_url($lang.'/classicverify'); ?>",
-          method:"POST",
-          data:{username, password},
-          dataType:"text",
-          beforeSend: function(){
-            $.amaran({
-              'theme': 'awesome info',
-              'content': {
-                title: '<?= $this->lang->line('notification_title_info'); ?>',
-                message: '<?= $this->lang->line('notification_checking'); ?>',
-                info: '',
-                icon: 'fas fa-sign-in-alt'
-              },
-              'delay': 5000,
-              'position': 'top right',
-              'inEffect': 'slideRight',
-              'outEffect': 'slideRight'
+          url: "<?= base_url($lang . '/classicverify'); ?>",
+          method: "POST",
+          data: {
+            username,
+            password
+          },
+          dataType: "text",
+
+          beforeSend: function() {
+            Swal.fire({
+              icon: 'info',
+              title: '<?= $this->lang->line('notification_title_info'); ?>',
+              text: '<?= $this->lang->line('notification_checking'); ?>',
+              showConfirmButton: false,
+              timer: 5000
             });
           },
-          success:function(response){
-            if(!response)
+
+          success: function(response) {
+            if (!response)
               alert(response);
 
             if (response == 'uspErr') {
-              $.amaran({
-                'theme': 'awesome error',
-                'content': {
-                  title: '<?= $this->lang->line('notification_title_error'); ?>',
-                  message: '<?= $this->lang->line('notification_user_error'); ?>',
-                  info: '',
-                  icon: 'fas fa-times-circle'
-                },
-                'delay': 5000,
-                'position': 'top right',
-                'inEffect': 'slideRight',
-                'outEffect': 'slideRight'
+              Swal.fire({
+                icon: 'error',
+                title: '<?= $this->lang->line('notification_title_error'); ?>',
+                text: '<?= $this->lang->line('notification_user_error'); ?>',
+                showConfirmButton: true,
               });
+
               $('#loginForm')[0].reset();
               return false;
             }
 
             if (response) {
-              $.amaran({
-                'theme': 'awesome ok',
-                  'content': {
-                  title: '<?= $this->lang->line('notification_title_success'); ?>',
-                  message: '<?= $this->lang->line('notification_redirection'); ?>',
-                  info: '',
-                  icon: 'fas fa-check-circle'
-                },
-                'delay': 5000,
-                'position': 'top right',
-                'inEffect': 'slideRight',
-                'outEffect': 'slideRight'
+              Swal.fire({
+                icon: 'success',
+                title: '<?= $this->lang->line('notification_title_success'); ?>',
+                text: '<?= $this->lang->line('notification_redirection'); ?>',
+                showConfirmButton: true,
+                timer: 50000
               });
             }
             $('#loginForm')[0].reset();
