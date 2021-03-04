5f2f9cc980e22e1ca1c828e1b5fd38f5fcdf56a0
Darthar <duelista16@gmail.com>
2021-03-04 15:14:57 +0100

    Revert "gitIgnore"
    This reverts commit 6ddddaa400f9c80e4d5b37adb9c93616951e426e.

---
+++ migration/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Configuration of the application folder
+$config['application'] = '../application';
+
+// Name settings to be replaced
+$config['language'] = '%LANGUAGE%';
+$config['hostname'] = '%HOSTNAME%';
+$config['username'] = '%USERNAME%';
+$config['password'] = '%PASSWORD%';
+$config['database'] = '%DATABASE%';
+$config['hostname2'] = '%HOSTNAME2%';
+$config['username2'] = '%USERNAME2%';
+$config['password2'] = '%PASSWORD2%';
+$config['database2'] = '%DATABASE2%';
