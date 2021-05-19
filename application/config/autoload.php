6d25e1dda2641628c3940dafa7a72230b931cfc9
Darthar <duelista16@gmail.com>
2021-05-19 17:27:34 +0200

    Remove license system
    Residual code from the old licensing system has been completely removed.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'form_validation', 'parser', 'template', 'Wowcms', 'cart');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'parser', 'template', 'cart');
 
 /*
 | -------------------------------------------------------------------
