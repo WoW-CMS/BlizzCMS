6a703819fc11c4744ce490337e75b2f3a716f7e7
Pagani Walter <paganiwalter@gmail.com>
2022-03-04 08:15:01 -0300

    Modifying blizzcms.php format
    A common format for file documentation is established. The FALSE variable is also changed to false and some default data, which may have been uploaded by mistake during a pull request or commit, is removed.

--- application/config/blizzcms.php
+++ application/config/blizzcms.php
@@ -3,105 +3,104 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  *
- * Website Name
+ *  Website Name
  *
- * Write the name of your website this will appear by default.
+ *  Write the name of your website this will appear by default.
  *
 */
-$config['website_name'] = 'Test';
+$config['website_name'] = '';
 
 /**
  *
- * Timezone
+ *  Timezone
  *
- * http://php.net/manual/en/timezones.php
+ *  http://php.net/manual/en/timezones.php
  *
 */
 $config['timezone'] = 'GMT';
 
 /**
  *
- * Maintenance Mode
+ *  Maintenance Mode
  *
- * 1 = Enable | 0 = Disable
+ *  1 = Enable | 0 = Disable
  *
 */
 $config['maintenance_mode'] = '0';
 
 /**
  *
- * Invitation Discord
+ *  Invitation Discord
  *
- * Write the invitation of your discord channel.
+ *  Write the invitation of your discord channel.
  *
 */
 $config['discord_invitation'] = '';
 
 /**
  *
- * Realmlist
+ *  Realmlist
  *
- * Write the realmlist used on your server to publish it on the website.
+ *  Write the realmlist used on your server to publish it on the website.
  *
 */
-$config['realmlist'] = 'test.test';
+$config['realmlist'] = '';
 
 /**
  *  Bnet enabled?
- * 
  *
+ *  true for Emulators BattleNet.
+ *  false for not bnetserver
  */
-
-$config['bnet_enabled'] = false; // Default: True for Emulators BattleNet and false for not bnetserver
+$config['bnet_enabled'] = false;
 
  /**
  *  Emulator
- * 
+ *
  *
  *  srp6 = For Emulators (SRP6 Compatibility)
  *  old-trinity =  Trinity Core not SRP6  (Sha_pass_hash Compatibility)
  *  hex = For emulators Mangos  (HEX6 Compatibility)
- *  
+ *
  */
-
 $config['emulator'] = 'srp6';
 
 /**
  *
- * Expansion Supported
+ *  Expansion Supported
  *
- * Select the expansion that your website will use among these options:
+ *  Select the expansion that your website will use among these options:
  *
- * 1 = Vanilla
- * 2 = The Burning Crusade
- * 3 = Wrath of the Lich King
- * 4 = Cataclysm
- * 5 = Mist of Pandaria
- * 6 = Warlords of Draenor
- * 7 = Legion
- * 8 = Battle for Azeroth
- * 9 = Shadowlands
+ *  1 = Vanilla
+ *  2 = The Burning Crusade
+ *  3 = Wrath of the Lich King
+ *  4 = Cataclysm
+ *  5 = Mist of Pandaria
+ *  6 = Warlords of Draenor
+ *  7 = Legion
+ *  8 = Battle for Azeroth
+ *  9 = Shadowlands
  *
 */
-$config['expansion'] = '3';
+$config['expansion'] = '';
 
 /**
  *
- * Theme Name
+ *  Theme Name
  *
- * Write the name of your theme
- * The name is the same as the main folder
- * The css must also have the same name
- * Default: default
+ *  Write the name of your theme
+ *  The name is the same as the main folder
+ *  The css must also have the same name
+ *  Default: default
  *
 */
 $config['theme_name'] = 'default';
 
 /**
  *
- * Social Links
+ *  Social Links
  *
- * Write the links for redirect to your social networks.
+ *  Write the links for redirect to your social networks.
  *
 */
 $config['social_facebook'] = '';
@@ -110,21 +109,21 @@ $config['social_youtube'] = '';
 
 /**
  *
- * Recaptcha (V2)
+ *  Recaptcha (V2)
  *
- * Write the necessary keys to enable recaptcha in the register
- * Use the following page to create the necessary keys.
- * https://www.google.com/recaptcha/admin#list
+ *  Write the necessary keys to enable recaptcha in the register
+ *  Use the following page to create the necessary keys.
+ *  https://www.google.com/recaptcha/admin#list
  *
 */
 $config['recaptcha_sitekey'] = '';
 
 /**
  *
- * SMTP
+ *  SMTP
  *
- * Write the necessary information for use SMTP to use in recovery password
- * and account activation.
+ *  Write the necessary information for use SMTP to use in recovery password
+ *  and account activation.
  *
 */
 $config['smtp_host'] = '';
@@ -135,9 +134,9 @@ $config['smtp_crypto'] = 'ssl';
 
 /**
  *
- * Email Settings
+ *  Email Settings
  *
- * Write the necessary information to use in sending emails.
+ *  Write the necessary information to use in sending emails.
  *
 */
 $config['email_settings_sender'] = '';
@@ -145,41 +144,39 @@ $config['email_settings_sender_name'] = '';
 
 /**
  *
- * Account Activation
+ *  Account Activation
  *
- * Enable or Disable the option to activate accounts by email.
+ *  Enable or Disable the option to activate accounts by email.
  *
- * TRUE  = Enable
- * FALSE = Disable
+ *  true  = Enable
+ *  false = Disable
  *
 */
-$config['account_activation_required'] = FALSE;
+$config['account_activation_required'] = false;
 
 /**
  *
- * Administrator Access Level
+ *  Administrator Access Level
  *
- * Minimum gmlevel to access to admin sections.
+ *  Minimum gmlevel to access to admin sections.
  *
 */
 $config['admin_access_level'] = '3';
 
 /**
  *
- * Moderator Access Level
+ *  Moderator Access Level
  *
- * Minimum gmlevel to access to mod sections.
+ *  Minimum gmlevel to access to mod sections.
  *
 */
 $config['mod_access_level'] = '2';
 
 /**
  *
- * Migrate Status
+ *  Migrate Status
  *
- * Warning: Don't change this configuration.
+ *  Warning: Don't change this configuration.
  *
 */
 $config['migrate_status'] = '1';
-
-
