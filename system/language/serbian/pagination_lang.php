5530da3b738f5ff5f18d053f0f4115fc9fa8344c
DZywolf <34007722+DZywolf@users.noreply.github.com>
2023-09-01 22:27:17 -0400

    chore: add languages to system

---
+++ system/language/serbian/pagination_lang.php
@@ -0,0 +1,16 @@
+<?php
+ /**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright	Novak Urošević
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('Nije dozvoljen direktan pristup');
+
+$lang['pagination_first_link'] = '&lsaquo; Prva';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_last_link'] = 'Zadnja &rsaquo;';
