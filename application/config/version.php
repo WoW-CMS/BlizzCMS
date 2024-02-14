13b39b9f086b79a342915d825931110640e1d692
sayghteight <sayghteight@gmail.com>
2024-02-14 16:29:49 +0100

    Update version.php

--- application/config/version.php
+++ application/config/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['cms_licenseKey'] = 'LMR9X-3PQK7-8W6Y2-ZA1B5';
+$config['cms_licenseKey'] = 'YOUR LICENSE';
 
 $config['cms_version'] = '2.0.0-beta.3';
 
