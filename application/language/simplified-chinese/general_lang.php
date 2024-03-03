c3b7d8f8249ca3b7c0b537d638b3c1bd57124a3d
wolone <71954466@qq.com>
2024-03-03 17:34:42 +0800

    Add simplified Chinese

---
+++ application/language/simplified-chinese/general_lang.php
@@ -0,0 +1,399 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['home'] = '首页';
+$lang['news'] = '新闻';
+$lang['menu'] = '菜单';
+$lang['forgot_password'] = '忘记密码';
+$lang['reset_password'] = '重置密码';
+$lang['user_panel'] = '用户中心';
+$lang['admin_panel'] = '管理面板';
+$lang['dashboard'] = '仪表盘';
+$lang['powered_by'] = 'Powered by';
+
+$lang['register'] = '注册';
+$lang['login'] = '登录';
+$lang['logout'] = '注销';
+$lang['reset'] = '重置';
+$lang['forgot_your_password'] = '忘记密码了吗？';
+$lang['remember'] = '记住我';
+$lang['validate_your_registration'] = '验证您的注册';
+$lang['dont_have_account'] = "还没有帐户？注册";
+$lang['already_have_account'] = '你已经有账户了吗？登录';
+$lang['fill_form_password_reset'] = '填写表格以重置密码';
+$lang['account_confirmation'] = '账户确认';
+$lang['here'] = '这里';
+$lang['submit'] = '提交';
+$lang['change'] = '更改';
+$lang['cancel'] = '取消';
+$lang['send'] = '发送';
+$lang['read_more'] = '阅读更多';
+$lang['delete'] = '删除';
+$lang['add'] = '添加';
+$lang['edit'] = '编辑';
+$lang['save'] = '保存';
+$lang['close'] = '关闭';
+$lang['reply'] = '回复';
+$lang['continue'] = '继续';
+$lang['go_cart'] = 'Go to cart';
+$lang['check'] = '检查';
+$lang['install'] = '安装';
+$lang['uninstall'] = '卸载';
+$lang['reload'] = '重新加载';
+$lang['force'] = '强制';
+$lang['filter'] = '筛选';
+$lang['search'] = '搜索';
+$lang['select'] = '选择';
+$lang['update'] = '更新';
+$lang['configure'] = '配置';
+$lang['restore'] = '修复';
+$lang['activate'] = '激活';
+$lang['deactivate'] = '停用';
+$lang['grant'] = 'Grant';
+$lang['back'] = 'Back';
+$lang['view'] = '查看';
+$lang['views'] = '查看';
+$lang['current'] = '当前';
+$lang['migrate'] = '迁移';
+$lang['username'] = '用户名';
+$lang['email'] = '邮箱地址';
+$lang['email_or_username'] = '邮箱/用户名';
+$lang['password'] = '密码';
+$lang['confirm_password'] = '确认密码';
+$lang['confirm_new_password'] = '确认新密码';
+$lang['current_password'] = '当前密码';
+$lang['new_password'] = '新密码';
+$lang['new_nickname'] = '新昵称';
+$lang['new_email'] = '新邮箱';
+$lang['current_nickname'] = '当前昵称';
+$lang['current_email'] = '当前邮箱';
+$lang['avatar'] = '头像';
+$lang['title'] = '标题';
+$lang['type'] = '类别';
+$lang['types'] = '类别';
+$lang['tag'] = 'Tag';
+$lang['tags'] = 'Tags';
+$lang['summary'] = '摘要';
+$lang['content'] = '内容';
+$lang['description'] = '描述';
+$lang['url'] = 'URL';
+$lang['slug'] = 'URL slug';
+$lang['highlight'] = 'Highlight';
+$lang['lock'] = 'Lock';
+$lang['unlock'] = 'Unlock';
+$lang['subject'] = 'Subject';
+$lang['nickname'] = '昵称';
+$lang['icon'] = '图标';
+$lang['category'] = '类别';
+$lang['categories'] = '类别';
+$lang['name'] = '名称';
+$lang['item'] = 'Item';
+$lang['items'] = 'Items';
+$lang['reason'] = 'Reason';
+$lang['gm_level'] = 'GM等级';
+$lang['parent'] = '父类';
+$lang['target'] = '目标';
+$lang['url_type'] = 'URL Type';
+$lang['path'] = 'Path';
+$lang['hostname'] = '主机名';
+$lang['database'] = '数据库';
+$lang['port'] = '端口';
+$lang['prefix'] = '前缀';
+$lang['maximum_capacity'] = '最大连接数';
+$lang['command'] = 'Command';
+$lang['emulator'] = '模拟器';
+$lang['faction'] = '阵营';
+$lang['id'] = 'ID';
+$lang['status'] = '状态';
+$lang['priority'] = 'Priority';
+$lang['severity'] = 'Severity';
+$lang['date'] = '日期';
+$lang['expires_at'] = 'Expires at';
+$lang['created_at'] = '创建时间';
+$lang['updated_at'] = 'Updated at';
+$lang['start_at'] = 'Start at';
+$lang['end_at'] = 'End at';
+$lang['author'] = 'Author';
+$lang['time'] = 'Time';
+$lang['realm'] = 'Realm';
+$lang['zone'] = 'Zone';
+$lang['character'] = 'Character';
+$lang['price'] = 'Price';
+$lang['quantity'] = 'Quantity';
+$lang['race'] = 'Race';
+$lang['class'] = 'Class';
+$lang['level'] = 'Level';
+$lang['money'] = 'Money';
+$lang['time_played'] = 'Time played';
+$lang['action'] = '操作';
+$lang['actions'] = '操作';
+$lang['event'] = 'Event';
+$lang['module'] = 'Module';
+$lang['hash'] = 'Hash';
+$lang['token'] = 'Token';
+$lang['code'] = 'Code';
+$lang['total'] = 'Total';
+$lang['total_dp'] = 'Total DP';
+$lang['total_vp'] = 'Total VP';
+$lang['guid'] = 'Guid';
+$lang['details'] = 'Details';
+$lang['value'] = 'Value';
+$lang['encryption'] = '加密';
+$lang['protocol'] = '协议';
+$lang['expansion'] = '扩展';
+$lang['normal'] = 'Normal';
+$lang['compact'] = 'Compact';
+$lang['default'] = 'Default';
+$lang['link'] = '链接';
+$lang['dropdown'] = '下拉菜单';
+$lang['image'] = 'Image';
+$lang['video'] = 'Video';
+$lang['iframe'] = 'Iframe';
+$lang['file'] = 'File';
+$lang['ssl'] = 'SSL';
+$lang['tls'] = 'TLS';
+$lang['dp'] = 'DP';
+$lang['vp'] = 'VP';
+$lang['new_tab'] = '新建选项卡/窗口';
+$lang['same_tab'] = '相同的选项卡/窗口';
+$lang['no_parent'] = '没有父类';
+$lang['set_default'] = '设置为默认值';
+$lang['file_not_loaded'] = '未加载文件';
+$lang['file_not_found'] = '找不到文件';
+$lang['image_not_loaded'] = '未加载图像';
+$lang['characters'] = 'Characters';
+$lang['guest'] = 'Guest';
+$lang['user'] = 'User';
+$lang['admin'] = 'Admin';
+$lang['banned_user'] = 'Banned user';
+$lang['bnet_authentication'] = 'Bnet身份验证';
+$lang['language'] = '语言';
+$lang['languages'] = '语言';
+$lang['site_name'] = '站点名称';
+$lang['realmlist'] = '领域列表';
+$lang['list'] = 'List';
+$lang['builder'] = '生成器';
+$lang['message'] = '消息';
+$lang['ip'] = 'IP地址';
+$lang['upload'] = '上传';
+$lang['version'] = '版本';
+$lang['versions'] = '版本';
+$lang['position'] = '位置';
+$lang['captcha'] = '验证码';
+$lang['size'] = '尺寸';
+$lang['max_size'] = '最大尺寸';
+$lang['color'] = '颜色';
+$lang['background'] = '背景';
+$lang['role'] = '权限组';
+$lang['by'] = 'By';
+$lang['preview'] = 'Preview';
+$lang['frequency'] = 'Frequency';
+$lang['from'] = 'From';
+$lang['to'] = 'To';
+$lang['minutes'] = '分钟';
+$lang['hours'] = '小时';
+$lang['days'] = '日';
+$lang['months'] = '月';
+$lang['years'] = '年';
+$lang['both'] = 'Both';
+$lang['currency'] = 'Currency';
+$lang['object'] = 'Object';
+$lang['key'] = 'Key';
+$lang['none'] = 'None';
+$lang['period'] = 'Period';
+$lang['permanent'] = 'Permanent';
+$lang['temporary'] = 'Temporary';
+$lang['timespan'] = 'Timespan';
+$lang['endless'] = 'Endless';
+$lang['visible'] = 'Visible';
+$lang['hidden'] = 'Hidden';
+$lang['terms'] = 'Terms';
+$lang['theme'] = '主题';
+$lang['themes'] = '主题';
+
+$lang['offline'] = '离线';
+$lang['online'] = '在线';
+$lang['unknown'] = '未知';
+$lang['completed'] = 'Completed';
+$lang['canceled'] = 'Canceled';
+$lang['declined'] = 'Declined';
+$lang['pending'] = 'Pending';
+$lang['processing'] = 'Processing';
+$lang['enable'] = '启用';
+$lang['disable'] = '禁用';
+$lang['error'] = '错误';
+$lang['warning'] = '警告';
+$lang['information'] = '信息';
+$lang['success'] = '成功';
+$lang['succeeded'] = 'Succeeded';
+$lang['failed'] = 'Failed';
+$lang['open'] = 'Open';
+$lang['closed'] = 'Closed';
+$lang['active'] = '激活';
+$lang['inactive'] = '未激活';
+$lang['installed'] = '安装';
+$lang['not_installed'] = '未安装';
+
+$lang['read_and_agree'] = '我已经阅读并同意';
+$lang['terms_service'] = '服务条款';
+$lang['privacy_policy'] = '隐私政策';
+$lang['and'] = '和';
+$lang['or'] = 'or';
+$lang['rights_reserved'] = 'All rights reserved.';
+
+$lang['comment'] = '评论';
+$lang['comments'] = '评论';
+$lang['latest_comments'] = '最新评论';
+$lang['add_comment'] = '添加评论';
+$lang['edit_comment'] = '编辑评论';
+$lang['latest_news'] = '最新动态';
+$lang['realm_status'] = '服务器状态';
+$lang['social_networks'] = '社交网络';
+$lang['published_on'] = '发布时间';
+$lang['written_on'] = 'Written on';
+$lang['registered_on'] = '注册时间';
+$lang['donation_points'] = '捐赠积分';
+$lang['voting_points'] = '投票积分';
+$lang['facebook'] = 'Facebook';
+$lang['discord'] = 'Discord';
+$lang['reddit'] = 'Reddit';
+$lang['twitch'] = 'Twitch';
+$lang['x_twitter'] = 'X (Twitter)';
+$lang['youtube'] = 'YouTube';
+$lang['cart_have_products'] = 'You have <strong>%1$d</strong> product(s) in your shopping cart';
+$lang['cart_is_empty'] = '购物车是空的';
+
+/**
+ * Factions
+*/
+$lang['alliance'] = '联盟';
+$lang['horde'] = '部落';
+$lang['neutral'] = '中立';
+
+/**
+ * Selects
+*/
+$lang['select_type'] = '选择类型';
+$lang['select_priority'] = '选择优先级';
+$lang['select_severity'] = '选择严重性';
+$lang['select_category'] = '选择类别';
+$lang['select_tag'] = '选择标记';
+$lang['select_realm'] = '选择一个领域';
+$lang['select_character'] = '选择一个角色';
+$lang['select_item'] = '选择一个物品';
+$lang['select_parent'] = '选择父类';
+$lang['select_target'] = '选择目标';
+$lang['select_language'] = '选择语言';
+$lang['select_expansion'] = '选择扩展';
+$lang['select_emulator'] = '选择模拟器';
+$lang['select_status'] = '选择状态';
+$lang['select_encryption'] = '选择加密';
+$lang['select_protocol'] = '选择协议';
+$lang['select_option'] = '选择一个选项';
+$lang['select_currency'] = '选择货币';
+$lang['select_theme'] = '选择主题';
+$lang['select_size'] = '选择尺寸';
+
+$lang['all_types'] = '所有类型';
+$lang['all_realms'] = '所有领域';
+$lang['all_characters'] = '所有角色';
+$lang['all_categories'] = '所有类别';
+
+/**
+ * DateTime
+*/
+$lang['datetime_pattern'] = "Y-M-d";
+$lang['datetime_full_pattern'] = "Y-M-d,G:i:s";
+
+/**
+ * Alerts
+*/
+$lang['alert_login_failed'] = '身份验证失败。您已使用%1$d次<strong>%2$d</strong>登录尝试。';
+$lang['alert_login_attempts_exhausted'] = '登录尝试次数已用完。请在<strong>%1$d</strong>分钟后重试。';
+$lang['alert_registration_success'] = '帐户已成功创建！';
+$lang['alert_registration_pending'] = '您的帐户即将就绪！请检查您的电子邮件收件箱，并查找我们发送的电子邮件以验证它。';
+$lang['alert_forgot_success'] = '已将一封电子邮件发送到您的电子邮件地址以重置您的密码。';
+$lang['alert_registration_validated'] = '该帐户已验证，现在您可以登录。';
+$lang['alert_reset_password'] = '帐户密码已重置，现在您可以登录。';
+$lang['alert_token_invalid'] = '所使用的令牌已过期或无效。';
+$lang['alert_password_invalid'] = '密码不正确。请再试一次！';
+$lang['alert_nickname_exists'] = '昵称已在使用中。';
+$lang['alert_username_exists'] = '用户名已在使用中。';
+$lang['alert_email_exists'] = '电子邮件已在使用中。';
+$lang['alert_comment_published'] = '您的评论已发布。';
+$lang['alert_comment_updated'] = '评论已更新。';
+$lang['alert_comment_deleted'] = '该评论已被删除。';
+$lang['alert_account_permanently_banned'] = '您的帐户被永久禁止。';
+$lang['alert_email_blocked'] = '使用的电子邮件地址被阻止。请尝试使用另一个。';
+
+/**
+ * CMS
+*/
+$lang['cms_updater_not_connected'] = '无法连接以检查更新，请稍后再试';
+$lang['cms_updater_version_not_found'] = '找不到可用的新版本';
+$lang['cms_updater_failed'] = '尝试更新时出错！有关详细信息，请参阅日志文件';
+$lang['cms_updater_success'] = 'CMS已更新到版本<strong>%1$s</strong>';
+$lang['cms_migration_not_files'] = '没有待处理的迁移文件';
+$lang['cms_migration_processed'] = '已处理迁移文件';
+
+/**
+ * Errors
+*/
+$lang['error_auth_connection'] = '无法建立到auth数据库的连接';
+$lang['error_characters_connection'] = '无法建立与characters数据库的连接';
+$lang['error_characters_db_not_found'] = '找不到用于与characters数据库建立连接的数据';
+
+/**
+ * Emails
+*/
+$lang['email_account_confirmation'] = '要确认创建与此电子邮件地址关联的帐户，请单击以下链接：';
+$lang['email_reset_password'] = '已请求重置与此电子邮件地址关联的帐户的密码。要继续，请单击链接：';
+$lang['email_note_time_limit'] = '此电子邮件中的链接在自操作之日起的<strong>12小时</strong>内有效。如果您对此有任何问题，请联系我们的支持团队。';
+
+/**
+ * Exceptions
+*/
+$lang['exception_error_404'] = 'Error 404';
+$lang['exception_page_not_found'] = '找不到页面';
+$lang['exception_page_requested_not_found'] = '找不到您请求的页面';
+$lang['exception_permission_rejected'] = '许可被拒绝';
+$lang['exception_no_access_permission'] = '您没有访问此页面的权限！';
+$lang['exception_no_action_permission'] = '您没有执行此操作的权限';
+$lang['exception_must_be_logged_in'] = '您必须登录才能查看此页面！';
+$lang['exception_already_logged_in'] = '您已经登录，因此不再具有查看此页面的权限';
+
+/**
+ * Install
+*/
+$lang['installation'] = '安装';
+$lang['install_welcome'] = '<strong>欢迎</strong>我们很高兴您正在安装BlizzCMS。首先，将检查必要的要求以继续执行以下步骤。';
+$lang['install_requirements_check'] = '需求检查';
+$lang['install_php_extensions'] = 'PHP扩展';
+$lang['install_dependencies'] = '依赖项';
+$lang['install_permissions'] = '权限';
+$lang['install_required_version'] = '安装所需的最低版本7.4.x或建议的更新版本';
+$lang['install_required_extensions'] = '安装所需的PHP扩展';
+$lang['install_missing_extensions'] = '安装以下缺失的扩展插件';
+$lang['install_required_dependencies'] = '通过composer安装所需的包（依赖项管理）';
+$lang['install_missing_composer'] = '安装 <strong>composer</strong>';
+$lang['install_missing_dependencies'] = '打开终端在站点的根目录中运行以下命令';
+$lang['install_required_permissions'] = '对文件夹/文件设置所需的可写权限';
+$lang['install_missing_permissions'] = '为所有文件夹设置755权限，为所有文件设置644权限';
+$lang['install_how_comply_requirement'] = '如何遵守要求？';
+$lang['install_cms_database'] = 'CMS数据库';
+$lang['install_auth_database'] = 'Auth/Realmd数据库';
+$lang['install_database_note'] = '输入将用于建立与数据库的连接的数据';
+$lang['install_main_settings'] = '主要设置';
+$lang['install_main_settings_note'] = '输入安装过程中需要使用的以下主要设置';
+$lang['install_fresh_install'] = '全新安装';
+$lang['install_fresh_install_note'] = '如果选择<strong>全新安装</strong>，将创建一个具有管理员角色的新帐户（该角色仅适用于网站）。';
+$lang['install_previous_data'] = '现有数据';
+$lang['install_previous_data_note'] = '如果选择<strong>现有数据</strong>，则会复制现有的帐户，并将管理员角色授予具有指定用户名的帐户。';
+$lang['install_previous_data_warning'] = '没有电子邮件的帐户将使用以下电子邮件模式在网站上创建<strong>username@localhost</strong>，因此更改它是用户的责任';
+$lang['install_admin_account'] = '管理员帐户';
+$lang['install_cms_db_failed'] = '无法与cms数据库建立连接';
+$lang['install_auth_db_failed'] = '无法与身份验证数据库建立连接';
+$lang['install_change_file_failed'] = '无法更改配置文件。确保您对文件/文件夹具有适当的权限';
+$lang['install_step_locked'] = '此步骤已完成，因此无法重做';
+$lang['install_find_account_failed'] = '无法找到具有指定用户名的帐户';
+$lang['install_require_gm_rank'] = '所示帐户的GM级别不等于或大于3';
---
+++ application/modules/user/language/simplified-chinese/user_lang.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['my_profile'] = 'My Profile';
+$lang['profile'] = 'Profile';
+$lang['avatar_photo_public'] = 'Your avatar photo is public.';
+$lang['security'] = 'Security';
+$lang['security_settings'] = 'Security settings';
+$lang['security_activity'] = 'Security activity';
+$lang['update_account_password'] = 'Update your account password by clicking here.';
+$lang['update_account_email'] = 'Update your account email address by clicking here.';
+
+$lang['welcome_back'] = 'Welcome back,';
+$lang['welcome_back_note'] = 'Here you will see the most relevant information about the status and activity of your account. To change your account settings, go to';
+
+/**
+ * Alerts
+*/
+$lang['alert_own_profile_not_updated'] = 'Your profile has not been updated since no changes were detected';
+$lang['alert_own_profile_updated'] = 'Your profile information has been updated';
+$lang['alert_own_email_changed'] = 'Your email address has been changed';
+$lang['alert_own_password_changed'] = 'Your password has been changed';
