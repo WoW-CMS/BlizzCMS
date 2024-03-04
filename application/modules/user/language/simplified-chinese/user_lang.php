7f4c868d171eb817cc42d03701c4010a1b1e6f88
wolone <48593469+wolone@users.noreply.github.com>
2024-03-04 21:51:33 +0800

    Add simplified Chinese (#142)

---
+++ application/modules/user/language/simplified-chinese/user_lang.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['my_profile'] = '我的资料';
+$lang['profile'] = '资料';
+$lang['avatar_photo_public'] = '你的头像照片是公开的';
+$lang['security'] = '安全中心';
+$lang['security_settings'] = '安全设置';
+$lang['security_activity'] = '安全动态';
+$lang['update_account_password'] = '单击此处更新您的帐户密码';
+$lang['update_account_email'] = '单击此处更新您的帐户电子邮件地址';
+
+$lang['welcome_back'] = '欢迎回来,';
+$lang['welcome_back_note'] = '在这里，您将看到有关帐户状态和活动的最相关信息。要更改您的帐户设置，请转到';
+
+/**
+ * Alerts
+*/
+$lang['alert_own_profile_not_updated'] = '由于未检测到任何更改，您的配置文件尚未更';
+$lang['alert_own_profile_updated'] = '您的个人资料信息已更新';
+$lang['alert_own_email_changed'] = '您的电子邮件地址已更改';
+$lang['alert_own_password_changed'] = '您的密码已更改';
