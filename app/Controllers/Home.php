8ea53b9380e8868df3ab61df25b3ec02faaf8fa5
Jean-Dv <mrjunior127@gmail.com>
2024-03-24 19:42:46 -0500

    feat: add method to controller for set lang

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,19 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    /**
+     * Change site language
+     * 
+     * @param string $locale
+     * @return void
+     */
+    public function lang($locale = null)
+    {
+        $this->multilanguage->setLanguage($locale);
+
+
+        $agent = $this->request->getUserAgent();
+        return redirect()->to($agent->getReferrer());
+    }
 }
